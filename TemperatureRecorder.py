4086d36e00613903d1a9a98c36d482163eace405
SardineBob <yushlai1987@gmail.com>
2020-06-22 20:03:31 +0800

    支援多筆溫度讀取與寫入
    1.已支援多筆溫度讀取，並寫入DB
    2.DB File已改為每年一個檔案

--- TemperatureRecorder.py
+++ TemperatureRecorder.py
@@ -2,23 +2,26 @@
 import time
 from utilset.ConfigUtil import ConfigUtil
 from component.Temperature import Temperature
-from utilset.TemperatureUtil import TemperatureUtil
 from component.WebAPI import WebAPI
 
+# 建立Web微服務
 WebAPI()
-
 # 取得設定檔的值
 TempCaptureTime = ConfigUtil().TempCaptureTime
+Thermometer = ConfigUtil().Thermometer
+# 根據設定的溫控棒支數，逐一初始化
+temperature = []
+for item in Thermometer:
+    temperature.append(Temperature(item))
 # 開始無盡的根據設定循環秒數去擷取溫度數據並寫到SQLLite
 active = True
-temperature = Temperature()  # 溫度擷取物件
-TemperatureUtil = TemperatureUtil()  # 溫度紀錄物件
 print('開始紀錄溫度(每' + str(TempCaptureTime) + '秒)')
 try:
     while active:
-        temp = temperature.getTemperature()
-        TemperatureUtil.writeTemperature(temp)
-        print('目前溫度：' + str(temp))
+        for item in temperature:
+            temp = item.getTemperature()
+            item.writeTemperature(temp)
+            print('設備名稱：' + str(item.getName()) + '，目前溫度：' + str(temp))
         time.sleep(TempCaptureTime)
 except KeyboardInterrupt:
     active = False
--- utilset/TemperatureUtil.py
+++ utilset/TemperatureUtil.py
@@ -3,10 +3,11 @@ from datetime import datetime
 from utilset.SqlLiteUtil import SqlLiteUtil
 
 
-# 將取到的溫度紀錄在DB
-# 每天的溫度紀錄，會集中在同一個sqlLite db 檔案，也就是每天都會開不同db檔案存放溫度紀錄
+# 將讀取到的溫度紀錄在DB
+# 每年的溫度紀錄，會集中在同一個sqlLite db 檔案，也就是每年都會開不同db檔案存放溫度紀錄
 class TemperatureUtil():
 
+    __dbPath = "dbfile"
     __dbFilePath = None
     __sqlLiteUtil = None
 
@@ -20,32 +21,24 @@ class TemperatureUtil():
         # 將溫度insert到DB
         self.__insertTemperature({'temperature': temperature})
 
-    # 檢查今天日期的DB是否已產生
+    # 檢查今年的DB是否已產生
     def __checkDB(self):
-        # 取得今天db檔案路徑
-        today = datetime.now().strftime('%Y%m%d')
-        dbPath, dbFile = self.__getDBFilePath(today)
+        # 取得今年db檔案路徑
+        dbFile = datetime.now().strftime('%Y') + '.db'
         # 檢查路徑是否存在，不存在則建立
-        if os.path.exists(dbPath) is False:
-            os.makedirs(dbPath)
+        if os.path.exists(self.__dbPath) is False:
+            os.makedirs(self.__dbPath)
         # 檢查今天DB檔案是否存在，不存在則開啟
-        self.__dbFilePath = os.path.join(dbPath, dbFile)
+        self.__dbFilePath = os.path.join(self.__dbPath, dbFile)
         if os.path.isfile(self.__dbFilePath) is False:
             self.__createDB()
-
-    # 取得今天的db檔案路徑(today format is 20200101)
-    def __getDBFilePath(self, today):
-        # 路徑的組成規則是:dbfile/西元年/月/年月日.db
-        path = os.path.join('dbfile', today[0:4], today[4:6])
-        filename = today + '.db'
-        return (path, filename)
 
     # 產生DB檔案
     def __createDB(self):
         command = "CREATE TABLE RecordList(\
             RecodeTime Text NOT NULL,\
             Temperature REAL NOT NULL,\
-            PRIMARY KEY (RecodeTime)\
+            PRIMARY KEY (RecodeTime,Temperature)\
         )"
         # do create db
         self.__sqlLiteUtil.Execute(self.__dbFilePath, command, [])
@@ -66,8 +59,8 @@ class TemperatureUtil():
 
     # select溫度資料(目前for one day)
     def selectTemperature(self, date):
-        dbPath, dbFile = self.__getDBFilePath(date)
-        dbFilePath = os.path.join(dbPath, dbFile)
+        dbFile = datetime.now().strftime('%Y') + '.db'
+        dbFilePath = os.path.join(self.__dbPath, dbFile)
         # 檢查這個日期有沒有db file
         if os.path.isfile(dbFilePath) is False:
             return str(date) + "這個日期無溫度數據資料。"
