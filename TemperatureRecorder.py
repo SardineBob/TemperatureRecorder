e0cc1d73b3974b28c899e90ea9252a21b99f90dd
SardineBob <yushlai1987@gmail.com>
2020-07-07 17:02:36 +0800

    加入透過OLED Display呈現溫度的功能

--- TemperatureRecorder.py
+++ TemperatureRecorder.py
@@ -3,6 +3,7 @@ import time
 from utilset.ConfigUtil import ConfigUtil
 from component.Temperature import Temperature
 from component.WebAPI import WebAPI
+from component.OLEDPrinter import OLEDPrinter
 
 # 取得設定檔的值
 TempCaptureTime = ConfigUtil().TempCaptureTime
@@ -11,6 +12,7 @@ Thermometer = ConfigUtil().Thermometer
 temperature = []
 for item in Thermometer:
     temperature.append(Temperature(item))
+oledPrinter = OLEDPrinter()
 # 建立Web微服務
 WebAPI({'temperature': temperature})
 # 開始無盡的根據設定循環秒數去擷取溫度數據並寫到SQLLite
@@ -18,10 +20,14 @@ active = True
 print('開始紀錄溫度(每' + str(TempCaptureTime) + '秒)')
 try:
     while active:
+        tempCollect = []
         for item in temperature:
             temp = item.getTemperature()
+            tempCollect.append((item.getName(), temp))
             item.writeTemperature(temp)
             print('設備名稱：' + str(item.getName()) + '，目前溫度：' + str(temp))
+        # 輸出到OLED螢幕
+        oledPrinter.print(tempCollect)
         time.sleep(TempCaptureTime)
 except KeyboardInterrupt:
     active = False
