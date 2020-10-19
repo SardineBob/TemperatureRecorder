bfb8f8c233b7d900d1ae9428aae9973d1ffc5a8b
SardineBob <yushlai1987@gmail.com>
2020-10-19 22:37:12 +0800

    完成接收來自arduino的溫度資料的機制

--- component/ArduinoReader.py
+++ component/ArduinoReader.py
@@ -6,6 +6,7 @@ import json
 import shutil
 import serial
 from utilset.ConfigUtil import ConfigUtil
+from tkinter import messagebox
 
 
 # Arduino資料接收器
@@ -14,6 +15,8 @@ class ArduinoReader():
 
     __deviceRootPath = None
     __buzzer = None
+    __serial = None
+    __serialInitSuccess = False
 
     # 初始化，建立執行緒，每秒持續接收來自arduino serial的溫度計資料
     def __init__(self, buzzer):
@@ -32,6 +35,13 @@ class ArduinoReader():
 
     # 初始化溫度文件資料，每次啟動程式將__deviceRootPath資料夾移除
     def __initPath(self):
+        # 準備讀取arduino的序列埠
+        try:
+            self.__serial = serial.Serial("COM3", 9600)  # 建立serial序列埠連線，序列埠名稱固定抓/dev/tty*，限制只能接一組arduino
+        except:
+            messagebox.showerror("未連接溫度接收器", "未連接溫度接收序列埠，請確認是否接上，按下確定後，系統將自動重新啟動。")
+            os.system("sudo reboot")
+            exit()
         # 根據讀取json筆數產生對應序號的檔案與資料夾路徑(仿溫度計接在樹梅派對於w1 device產生溫度的動作)
         if os.path.exists(self.__deviceRootPath) is True:
             shutil.rmtree(self.__deviceRootPath, ignore_errors=True)  # 移除舊有的device資料夾
@@ -40,10 +50,8 @@ class ArduinoReader():
 
     # 接收來自arduino serial的溫度計資料
     def __readData(self):
-        # 從serial接收json data
-        data = self.__readArduinoSerial()
-        # 將json data轉成array<object>
-        dataArray = json.loads(data)
+        # 從serial接收json data，已加工處理為array<object>
+        dataArray = self.__readArduinoSerial()
         # 根據讀取json筆數產生對應序號的檔案與資料夾路徑(仿溫度計接在樹梅派對於w1 device產生溫度的動作)
         for item in dataArray:
             # 讀取相關屬性值
@@ -63,5 +71,30 @@ class ArduinoReader():
     # 讀取來自Arduino Serial的資料
     def __readArduinoSerial(self):
         # 判斷執行環境為window，直接回傳測試資料
-        if platform.system() is "Windows":
-            return '[{"deviceID":1, "tempID": 0, "tempSerial":"23579A1257983476", "temp":27.53},{"deviceID":0, "tempID": 1, "tempSerial":"23579A1846783479", "temp":30.53},{"deviceID":0, "tempID": 2, "tempSerial":"53579A18A678B470", "temp":30.53}]'
+        # if platform.system() is "Windows":
+        #    return json.loads('[{"deviceID":1, "tempID": 0, "tempSerial":"23579A1257983476", "temp":27.53},{"deviceID":0, "tempID": 1, "tempSerial":"23579A1846783479", "temp":30.53},{"deviceID":0, "tempID": 2, "tempSerial":"53579A18A678B470", "temp":30.53}]')
+        # 確保取到一筆完整的溫度資料，才算截取成功
+        try:
+            jsonData = []
+            while len(jsonData) <= 0:
+                # 擷取一次serial所有資料，切割後，逐一轉為json物件，過程中，可能因資料輸出未完成就讀取而截斷資料，這狀況的資料就拋棄，這邊只保留最新一筆完整的溫度資料
+                data = self.__serial.read_all()
+                jsonData = self.__getJsonData(data)
+                time.sleep(0.5)
+        except:
+            messagebox.showerror("序列埠中斷", "序列埠已中斷，請確認USB接頭，按下確定後，系統將自動重新啟動。")
+            os.system("sudo reboot")
+            exit()
+
+        return jsonData
+
+    # 分析來自arduino的json資料，並保留最後一筆完整的溫度資料
+    def __getJsonData(self, data):
+        jsonData = []
+        for item in data.splitlines():
+            try:
+                jsonData = json.loads(item)
+            except:
+                # 因json字串資料輸出未完成，就被讀取之時間差，這種資料無法轉json物件就拋棄，僅保留完整溫度資料
+                continue
+        return jsonData
